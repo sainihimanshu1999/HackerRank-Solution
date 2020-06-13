772ada62150366d433c9aeaf743b11afbb9ce969
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-06-13 10:49:24 +0530

    Birthday Candles

---
+++ BirthdayCandles.py
@@ -0,0 +1,31 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+
+
+def birthdayCakeCandles(ar):
+    count = 0
+    a = max(ar)
+    for i in range(len(ar)):
+        if(ar[i] == a):
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
