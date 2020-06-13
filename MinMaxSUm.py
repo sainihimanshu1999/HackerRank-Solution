72cbd3d9a1b579c625efb925a9fe478fc9eb0a6e
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-06-13 10:37:44 +0530

    min-max-sol

---
+++ MinMaxSUm.py
@@ -0,0 +1,20 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def miniMaxSum(arr):
+    sum = 0
+    for i in range(len(arr)):
+        sum += arr[i]
+
+    print(sum-max(arr), sum-min(arr))
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum([1, 2, 3, 4, 5])
