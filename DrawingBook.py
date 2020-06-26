79000beadc00160a1b583f92fb05fd2df072542c
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-06-26 20:50:35 +0530

    drawingbook soln

---
+++ DrawingBook.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the pageCount function below.
+#
+
+
+def pageCount(n, p):
+    if(p <= n):
+        return min(p//2, n//2 - p//2)
+
+    n = int(input().strip())
+    p = int(input().strip())
+    result = pageCount(n, p)
+    print(result)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    p = int(input())
+
+    result = pageCount(n, p)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
