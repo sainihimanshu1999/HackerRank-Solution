76b92ae376c09860aea9b73f8d89630236946e54
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-08-22 13:41:51 +0530

    minimum swap 2solution

---
+++ minimumswap2.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+
+
+def minimumSwaps(arr):
+    count = 0
+    temp = [0]*len(arr)
+
+    for index, value in enumerate(arr):
+        temp[value-1] = index
+
+    for i in range(len(arr)):
+        if arr[i] != (i+1):
+            t = temp[i]
+            arr[i], arr[t] = i+1, arr[i]
+            temp[i], temp[arr[t]-1] = i, t
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = minimumSwaps(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
