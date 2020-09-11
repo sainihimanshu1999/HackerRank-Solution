ad1e9e450474782b06add3c0c66108e3890d56ec
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-11 20:46:34 +0530

    sherlockanagram solutions

---
+++ sherlockanagram.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+def sherlockAndAnagrams(string):
+    buckets = {}
+    for i in range(len(string)):
+        for j in range(1, len(string) - i + 1):
+            key = frozenset(Counter(string[i:i+j]).items()) # O(N) time key extract
+            buckets[key] = buckets.get(key, 0) + 1
+    count = 0
+    for key in buckets:
+        count += buckets[key] * (buckets[key]-1) // 2
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = sherlockAndAnagrams(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
