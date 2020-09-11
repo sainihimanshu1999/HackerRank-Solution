05ca8e2f95a3262764831e0ded578face9ac3b2c
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-09-11 09:56:39 +0530

    Ransom note solution

---
+++ Ransom_note.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the checkMagazine function below.
+def checkMagazine(magazine, note):
+    a = Counter(note) - Counter(magazine)
+    res = not bool(a)
+    if res != True:
+        print('No')
+    else:
+        print('Yes')
+
+    
+
+if __name__ == '__main__':
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
